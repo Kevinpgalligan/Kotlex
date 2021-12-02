c5230eac633291d074e810bafdd65de430bcd4cd
j-jzk <jjezek@protonmail.com>
2021-12-02 14:15:20 +0100

    Change class structure of Symbols

--- src/com/tugboat/regex/Symbol.kt
+++ src/com/tugboat/regex/Symbol.kt
@@ -3,19 +3,19 @@ package com.tugboat.regex
 /**
  * Represents a class of characters and can match on those characters.
  */
-abstract class Symbol {
+interface Symbol {
 
-    abstract fun matches(char: Char): Boolean
+    fun matches(char: Char): Boolean
 
-    data class RawCharacter(private val rawChar: Char): Symbol() {
+    data class RawCharacter(private val rawChar: Char): Symbol {
         override fun matches(char: Char): Boolean = (rawChar == char)
     }
 
-    object Dot: Symbol() {
+    object Dot: Symbol {
         override fun matches(char: Char): Boolean = true
     }
 
-    class AnyOf(unsortedCharacters: Iterable<Char>): Symbol() {
+    class AnyOf(unsortedCharacters: Iterable<Char>): Symbol {
         val characters = unsortedCharacters.sorted()
 
         public constructor(string: String): this(string.toList())
@@ -24,12 +24,12 @@ abstract class Symbol {
         override fun equals(other: Any?) = other is AnyOf && other.characters == this.characters
     }
 
-    class NoneOf(unsortedCharacters: Iterable<Char>): Symbol() {
-        val characters = unsortedCharacters.sorted()
+    open class LogicalNot(val symbol: Symbol): Symbol {
+        override fun matches(char: Char) = !symbol.matches(char)
+        override fun equals(other: Any?) = other is LogicalNot && other.symbol == this.symbol
+    }
 
+    class NoneOf(unsortedCharacters: Iterable<Char>): LogicalNot(AnyOf(unsortedCharacters)) {
         public constructor(string: String): this(string.toList())
-
-        override fun matches(char: Char) = characters.binarySearch(char) < 0
-        override fun equals(other: Any?) = other is NoneOf && other.characters == this.characters
     }
 }
